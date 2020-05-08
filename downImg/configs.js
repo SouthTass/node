7a829cf5db16016ed3a450720ab9d61c2759ce1d
wzt_home <wangzt@qll-times.com>
2020-05-09 01:05:23 +0800

    add

--- downImg/configs.js
+++ downImg/configs.js
@@ -1,15 +1,49 @@
 let mysql = require('mysql');
-let connection = mysql.createConnection({
-  host: '120.77.156.93',
-  user: 'wzt',
-  password: '147258',
-  database: 'test'
-});
-connection.connect();
+let http = require('http');
 
-let sql_text = `select name from user`;
-connection.query(sql_text, (err, res, fileds) => {
-  let a = JSON.stringify(res);
-  let b = JSON.parse(a);
-  console.log("查询到的数据", b, res);
-});
+let default_json_text = {
+  status: 0, 
+  message: '路径错误'
+}
+
+
+// function selectData(type){
+  
+  
+//   // return result;
+// }
+
+
+
+http.createServer(function(req, res){
+
+  let connection = mysql.createConnection({
+    host: '120.77.156.93',
+    user: 'wzt',
+    password: '147258',
+    database: 'test'
+  });
+
+  connection.connect();
+
+  let sql_text = `select name from user`;
+  let result;
+  
+  connection.query(sql_text, (err, res, fileds) => {
+    let a = JSON.stringify(res);
+    result = JSON.parse(a);
+    console.log("mysql", result);
+  });
+
+
+  
+  
+  setTimeout(() => {
+    console.log("http", result);
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.write(JSON.stringify(result));
+    res.end();
+  }, 3000);
+  
+  
+}).listen(3000);
