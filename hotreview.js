5da3eb1c8ab5d6be8f9b51d00eaee9bf3b93922d
wzt <wangzt@qll-times.com>
2020-09-09 16:22:52 +0800

    网易云热评增加擦剂速率

--- hotreview.js
+++ hotreview.js
@@ -15,11 +15,10 @@ function get(url) {
   superagent.get(url)
     .end(function (err, res) {
       let result = JSON.parse(res.text);
-      let sql = 'SELECT content FROM hotreview';
+      let sql = `SELECT source FROM hotreview WHERE source = "${result.newslist[0].source}"`;
       connection.query(sql, function (errq, resq) {
-        let index = resq.findIndex((item) => { return result.newslist[0].content == item.content });
-        console.log(index == -1 ? "唯一" : `重复${moment()}`);
-        if(index == -1){
+        if(resq.length < 1){
+          console.log(`次数: ${count} 唯一`);
           let addSql = 'INSERT INTO hotreview(source, content, create_time) VALUES(?, ?, ?)';
           let addSqlParams = [
             result.newslist[0].source,
@@ -28,20 +27,18 @@ function get(url) {
           ];
           connection.query(addSql, addSqlParams, function (errx, resx) {
             let timer = setTimeout(() => {
-              if(count == 50000){
-                connection.end();
-                return;
-              }
+              if(count == 50000) return connection.end();
               count++;
               get(url);
               clearTimeout(timer);
-            }, 300);
+            }, 100);
           });
         }else{
+          console.log(`次数: ${count} 重复内容，重新请求。${moment().format('YYYY年MM月DD日 HH点mm分ss秒')}`);
           let timeset = setTimeout(() => {
             get(url);
             clearTimeout(timeset);
-          }, 300);
+          }, 100);
         }
       })
     });
